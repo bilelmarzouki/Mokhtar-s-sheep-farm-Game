83a9fceb9f55e0f2f5b146a9b43ee63cd2df33a9
Bilel Marzouki <bilelmarzou@gmail.com>
2026-01-28 20:35:16 +0100

    add score and time and restart functionalities

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 const StartScreenNode = document.querySelector("#game-start-screen");
 const StartButtonNode = document.querySelector("#start-btn");
-
 const ScreenNode = document.querySelector("#game-screen");
 const BoxScreenNode = document.querySelector("#box-game");
-
 const EndScreenNode = document.querySelector("#game-end-screen");
 const RestartGameButtonNode = document.querySelector("#restart-btn");
 const timeRemainingContainer = document.getElementById("timeRemaining");
-timeRemainingContainer.style.position="absolute"
-timeRemainingContainer.style.top="50px"
-timeRemainingContainer.style.right="20px"
+timeRemainingContainer.style.position = "absolute";
+timeRemainingContainer.style.top = "50px";
+timeRemainingContainer.style.right = "20px";
+const scoreContainer = document.getElementById("score");
+scoreContainer.style.position = "absolute";
+scoreContainer.style.top = "50px";
+scoreContainer.style.left = "40px";
+scoreContainer.style.fontSize = "30px";
+scoreContainer.style.fontWeight = "bold";
+const resultContainer = document.querySelector("#result");
 
 //* GLOBAL GAME VARIABLES
+let WinOrLoose = "";
 let farmerObj = null;
 let dogObj = null;
 let sheepArr = [];
@@ -29,10 +35,26 @@ let gateOpen4 = false;
 let numberGateOpenY3 = 0;
 let numberGateOpenY4 = 0;
 let caught = false;
-let caughtSheep=null
-let AllSheep=[]
+let caughtSheep = null;
+let AllSheep = [];
+const quizDuration = 120;
+let timeRemaining = quizDuration;
+let timeLimit = quizDuration;
+let countSheepInBarn = 0;
 StartScreenNode.style.display = "flex";
 ScreenNode.style.display = "none";
+let sheepSpawnId = null;
+let gameId = null;
+/*let fences = [
+  new Fence(700, 500),
+  new Fence(700, 310),
+  new Fence(780, 500),
+  new Fence(780, 310),
+  new Fence(830, 445, "vertical"),
+  new Fence(830, 365, "vertical"),
+  new Fence(640, 445, "vertical", gateOpen3, numberGateOpenY3),
+  new Fence(640, 365, "vertical", gateOpen4, numberGateOpenY4),
+];*/
 
 //* GLOBAL GAME FUNCTIONS
 function startGame() {
@@ -44,33 +66,50 @@ function startGame() {
   console.log(dogObj);
   makeSheep();
   barnConstruction();
-  setInterval(gameActions, Math.round(1000 / 60));
-  setInterval(sheepSpawn, 20000);
+  let id = setTimeout(() => {
+    let timer = setInterval(() => {
+      timeRemaining -= 1;
+      if (timeRemaining <= 0) {
+        //quiz.hasEnded();
+        gameOver();
+        //showResults();
+        //Won Or Loose
+        const minutes = Math.floor(timeLimit / 60)
+          .toString()
+          .padStart(2, "0");
+        const seconds = (timeLimit % 60).toString().padStart(2, "0");
+        timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+        //endView.style.display = "block";
+        // quizView.style.display = "none";
+        clearInterval(timer);
+      }
+      const minutes = Math.floor(timeRemaining / 60)
+        .toString()
+        .padStart(2, "0");
+      const seconds = (timeRemaining % 60).toString().padStart(2, "0");
+      timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+      timeRemainingContainer.style.fontSize = "30px";
+      timeRemainingContainer.style.fontWeight = "bold";
+    }, 1000);
+  }, 0);
+
+  gameId = setInterval(gameActions, Math.round(1000 / 60));
+  sheepSpawnId = setInterval(sheepSpawn, 20000);
 }
 
 function gameActions() {
-  //farmerObj.moveForward()
-  /*farmerObj.moveBackward()
-  farmerObj.moveUp()
-  farmerObj.moveDown()*/
-  //dogObj.moveForward()
-  /*dogObj.moveBackward()
-  dogObj.moveUp()
-  dogObj.moveDown()*/
   sheepArr.forEach((sheep) => {
     sheep.walk();
-    /*sheep.moveBackward()
-     sheep.moveDown()
-     sheep.moveUp()*/
   });
   sheepArrSpawn.forEach((sheepSpawn) => {
     sheepSpawn.walk();
-    /* sheepSpawn.moveBackward()
-     sheepSpawn.moveDown()
-     sheepSpawn.moveUp()*/
   });
-   collisionFarmerSheep();
+  collisionFarmerSheep();
+  //collisionSheepBarn()
+  myScore();
+  scoreContainer.innerText = `Score:${countSheepInBarn}`;
 }
+
 function makeSheep() {
   let randomPositionX = Math.floor(Math.random() * 150) + 250;
   let randomPositionY = Math.floor(Math.random() * 150) + 100;
@@ -141,23 +180,22 @@ function checkOpenCloseGate4() {
     verticalFence4.closeTheGate();
   }
 }
-function getAlltheSheep(){
-   return sheepArr.concat(sheepArrSpawn)
+function getAlltheSheep() {
+  return sheepArr.concat(sheepArrSpawn);
 }
 
 function collisionFarmerSheep() {
-  if (caught) return  
-  AllSheep=getAlltheSheep()
+  if (caught) return;
+  AllSheep = getAlltheSheep();
   for (let i = 0; i < AllSheep.length; i++) {
-    
     let isColliding = checkCollision(farmerObj, AllSheep[i]);
-    if (isColliding && AllSheep[i].isInBarn===false) {
-      caughtSheep=AllSheep[i]
-      AllSheep[i].isCaught = true
+    if (isColliding && AllSheep[i].isInBarn === false) {
+      caughtSheep = AllSheep[i];
+      AllSheep[i].isCaught = true;
       AllSheep[i].x = farmerObj.x + 10;
-      AllSheep[i].node.style.left=`${AllSheep[i].x}px`
+      AllSheep[i].node.style.left = `${AllSheep[i].x}px`;
       AllSheep[i].y = farmerObj.y + 10;
-      AllSheep[i].node.style.top=`${AllSheep[i].y}px`
+      AllSheep[i].node.style.top = `${AllSheep[i].y}px`;
       // sheepArr[i].speed= farmerObj.speed
       break;
     }
@@ -165,29 +203,41 @@ function collisionFarmerSheep() {
 }
 
 //I will make it as a Bonus it's Not that important
-function collisionFarmerBarn() {
-  // birdObj
-  sheepArr.forEach((eachSheepObj) => {
-    // eachObstacleObj
-    let isColliding = checkCollision(farmerObj, eachSheepObj);
-    if (isColliding) {
-      console.log("the farmer has collided with the Barn ");
-      //gameOver()
-    }
+/*function collisionFarmerBarn() {
+  AllSheep.forEach((eachSheepObj) => {
+    let lastMoveSheepX = eachSheepObj.x;
+    let lastMoveSheepY = eachSheepObj.y;
+    eachSheepObj.walk();
+    fences.forEach((fence) => {
+      let isColliding = checkCollision(fence, eachSheepObj);
+      if (isColliding) {
+        eachSheepObj.x = lastMoveSheepX;
+        eachSheepObj.y = lastMoveSheepY;
+      }
+    });
   });
 }
+*/
 // I will make it as a Bonus it's Not that important
-function collisionSheepBarn() {
-  // birdObj
-  sheepArr.forEach((eachSheepObj) => {
-    // eachObstacleObj
-    let isColliding = checkCollision(Fence, eachSheepObj);
-    if (isColliding) {
-      console.log("the sheep has collided with the Barn");
-      //gameOver()
-    }
+/*function collisionSheepBarn() {
+  AllSheep.forEach((eachSheepObj) => {
+    let lastMoveSheepX = eachSheepObj.x;
+    let lastMoveSheepY = eachSheepObj.y;
+    //eachSheepObj.walk();
+    fences.forEach((fence) => {
+      if (!eachSheepObj.isCaught) {
+        let isColliding = checkCollision(fence, eachSheepObj);
+        if (isColliding) {
+          eachSheepObj.x = lastMoveSheepX;
+          eachSheepObj.y = lastMoveSheepY;
+          eachSheepObj.node.style.left = `${eachSheepObj.x} px`;
+          eachSheepObj.node.style.top = `${eachSheepObj.y}px`;
+        }
+      }
+    });
   });
 }
+*/
 
 function checkCollision(obj1, obj2) {
   // return true if both objects collide
@@ -199,9 +249,40 @@ function checkCollision(obj1, obj2) {
     obj1.y + obj1.height > obj2.y
   );
 }
+function myScore() {
+  return countSheepInBarn;
+}
+function resetGame() {
+  ScreenNode.innerHTML = "";
+}
 
+function gameOver() {
+  clearInterval(gameId);
+  clearInterval(sheepSpawnId);
+  clearTimeout(id);
+  ScreenNode.style.display = "none";
+  EndScreenNode.style.display = "flex";
+
+  if (countSheepInBarn > AllSheep.length / 2) {
+    resultContainer.innerText = `You catched ${countSheepInBarn} out of ${AllSheep.length} sheep in the field!
+       Congratulation You Win
+    `;
+  } else {
+    resultContainer.innerText = `You catched ${countSheepInBarn} out of ${AllSheep.length} sheep in the field!
+        Oops You Loose
+    `;
+  }
+}
 //* EVENT LISTENERS
 StartScreenNode.addEventListener("click", startGame);
+RestartGameButtonNode.addEventListener("click", () => {
+  EndScreenNode.style.display = "none";
+  //ScreenNode.style.display = "flex";
+  //resetGame();
+  //startGame();
+  ScreenNode.style.display = "flex";
+});
+
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight") {
     farmerObj.moveForward();
@@ -215,84 +296,11 @@ document.addEventListener("keydown", (event) => {
 
   checkOpenCloseGate3();
   checkOpenCloseGate4();
-  //collisionFarmerSheep();
 });
-document.addEventListener("keydown",(event)=>{
-    if(event.key ===" "){
-        caught=false
-        caughtSheep.isInBarn=true
-    }
-})
-/*document.addEventListener("keydown",(event)=>{
-    if(event.key==="ArrowRight"){
-        dogObj.moveForward() 
-    }else if(event.key==="ArrowLeft"){
-        dogObj.moveBackward()
-    }else if(event.key==="ArrowUp"){
-        dogObj.moveUp()
-    }else if(event.key==="ArrowDown"){
-        dogObj.moveDown()
-    }
-})*/
-
-//const fence3Y= verticalFence3.y
-//const nearGate3Y= farmerPositionY<=fence3Y-10 && farmerPositionY>=fence3Y-80
-
-/*else if(verticalFence3.isOpen && !nearGate3X && !nearGateY){
-       verticalFence3.numberGateOpenY=-65
-       verticalFence3.closeTheGate()
-    }*/
-/*else if(verticalFence4.isOpen && !nearGate4X && !nearGateY){
-       verticalFence4.numberGateOpenY=55
-       verticalFence4.closeTheGate()
-    }*/
-
-/*
-function checkGateOpen(){
-    const farmerPositionX=farmerObj.x +farmerObj.width;
-    const farmerPositionY=farmerObj.y +farmerObj.height;
-    const fence3X=verticalFence3.x
-    const fence4X=verticalFence4.x
-  
-    const nearGateY= 310<=farmerPositionY && farmerPositionY<=500
-    const nearGate3X= farmerPositionX>= fence3X - 10 && farmerPositionX <= fence3X + 10
-    const nearGate4X= farmerPositionX>= fence4X - 10 && farmerPositionX <= fence4X + 10
-    //Gate 3
-    if(!verticalFence3.isOpen && nearGate3X && nearGateY){
-       verticalFence3.numberGateOpenY=65
-       verticalFence3.openTheGate()
-    }
-    //Gate 4
-    if(!verticalFence4.isOpen && nearGate4X && nearGateY){
-       verticalFence4.numberGateOpenY=-55
-       verticalFence4.openTheGate()
-    }
-}
-function checkGateClose(){
-    const farmerPositionX=farmerObj.x +farmerObj.width;
-    const farmerPositionY=farmerObj.y +farmerObj.height;
-    const fence3X=verticalFence3.x
-    const fence4X=verticalFence4.x
-  
-    const nearGateY= 310<=farmerPositionY && farmerPositionY<=500
-    const nearGate3X= farmerPositionX>= fence3X - 10 && farmerPositionX <= fence3X + 10
-    const nearGate4X= farmerPositionX>= fence4X - 10 && farmerPositionX <= fence4X + 10
-    //Gate 3
-    if(verticalFence3.isOpen && (!nearGate3X || !nearGateY)){
-       verticalFence3.numberGateOpenY=65
-       verticalFence3.closeTheGate()
-    }
-    //Gate 4
-    if(verticalFence4.isOpen && (!nearGate4X || !nearGateY)){
-       verticalFence4.numberGateOpenY=-55
-       verticalFence4.closeTheGate()
-    }
-}*/
-/*function sheepSpawn(){
-    let randomPositionX=Math.floor(Math.random()*100)+500
-    let randomPositionY=Math.floor(Math.random()*100)+200
-    let sheepSpawn1=new Sheep(randomPositionX, randomPositionY)
-    sheepArrSpawn.push(sheepSpawn1)
-    let sheepSpawn2=new Sheep(randomPositionX, randomPositionY)
-    sheepArrSpawn.push(sheepSpawn2)
-} */
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    caught = false;
+    caughtSheep.isInBarn = true;
+    countSheepInBarn++;
+  }
+});
